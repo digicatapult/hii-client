c8ed2e5258bf98acf2bc485bfb48db1e07de430a
Jonathan Gray <jonmattgray@gmail.com>
2022-12-20 16:30:27 +0000

    add flash colour to cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -89,6 +89,7 @@ export default function Home() {
               background="#DCE5E7"
               height="5em"
               width="100%"
+              flashColor="#27847A"
               onClick={() => {}}
             />
           ))}
