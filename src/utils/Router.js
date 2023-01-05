fd8d6a0e75ffd3b5e582a63dadff4ea3a95540d4
Matthew Dean <matthew.dean@digicatapult.org.uk>
2023-01-05 11:03:46 +0000

    Fix basepath typo

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
-import { PUBLIC_BASEPATH } from './env'
+import { PUBLIC_BASE_PATH } from './env'
 
 export default function Router() {
   return (
-    <BrowserRouter basename={PUBLIC_BASEPATH}>
+    <BrowserRouter basename={PUBLIC_BASE_PATH}>
       <Routes>
         <Route exec path={'/'} element={<Home />} />
       </Routes>
