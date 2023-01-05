15f3bcbd8c718038ae80c30e6b8b92c1cf6b0a74
mattdean-digicatapult <mattdean-digicatapult@users.noreply.github.com>
2023-01-05 10:31:29 +0000

    deploy: 0dfa77984b7e4f20de650c484bdc1eb3654bf545

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const config={mapboxToken:"",mapboxStyle:"mapbox://styles/mapbox/light-v11"};Window.config||(Window.config={}),Window.config=Object.keys(config).reduce(((o,n)=>(o.hasOwnProperty(n)||(o[n]=config[n]),o)),{});
+const config={mapboxToken:"",mapboxStyle:"mapbox://styles/mapbox/light-v11",publicBasePath:"/"};Window.config||(Window.config={}),Window.config=Object.keys(config).reduce(((o,n)=>(o.hasOwnProperty(n)||(o[n]=config[n]),o)),{});
