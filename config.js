98bbdb08caf2955ee7a2e58dd64d0d96b1624e11
jonmattgray <jonmattgray@users.noreply.github.com>
2023-01-05 15:29:47 +0000

    deploy: e8869ab2b62bc4253bd1b8c3a33d8d51412848da

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const config={mapboxToken:"",mapboxStyle:"mapbox://styles/mapbox/light-v11",publicBasePath:"/"};Window.config||(Window.config={}),Window.config=Object.keys(config).reduce(((o,n)=>(o.hasOwnProperty(n)||(o[n]=config[n]),o)),{});
+const config={mapboxToken:"",mapboxStyle:"mapbox://styles/digicat-mapbox/clcj7eskl001q14o5bed5oam4",publicBasePath:"/"};Window.config||(Window.config={}),Window.config=Object.keys(config).reduce(((o,c)=>(o.hasOwnProperty(c)||(o[c]=config[c]),o)),{});
