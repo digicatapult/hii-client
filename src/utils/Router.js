0dfa77984b7e4f20de650c484bdc1eb3654bf545
Matthew Dean <matthew.dean@digicatapult.org.uk>
2023-01-05 10:25:33 +0000

    Add in configuration for base path so github pages hopefully works

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
+import { PUBLIC_BASEPATH } from './env'
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={PUBLIC_BASEPATH}>
       <Routes>
         <Route exec path={'/'} element={<Home />} />
       </Routes>
--- src/utils/env.js
+++ src/utils/env.js
@@ -3,5 +3,7 @@ const MAPBOX_STYLE =
   process.env.MAPBOX_STYLE ||
   Window.config.mapboxStyle ||
   'mapbox://styles/mapbox/light-v11'
+const PUBLIC_BASE_PATH =
+  process.env.PUBLIC_BASE_PATH || Window.config.publicBasePath || '/'
 
-export { MAPBOX_TOKEN, MAPBOX_STYLE }
+export { MAPBOX_TOKEN, MAPBOX_STYLE, PUBLIC_BASE_PATH }
