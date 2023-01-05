4c6e28a06dcce9853c463bb372bba3622542b12f
mattdean-digicatapult <mattdean-digicatapult@users.noreply.github.com>
2023-01-05 09:48:15 +0000

    deploy: 1ee291b66aa3ed18fbd4d748e744a930f46dbdd5

---
+++ config.js
@@ -0,0 +1,1 @@
+const config={mapboxToken:"",mapboxStyle:"mapbox://styles/mapbox/light-v11"};Window.config||(Window.config={}),Window.config=Object.keys(config).reduce(((o,n)=>(o.hasOwnProperty(n)||(o[n]=config[n]),o)),{});
