14773731a4a81dcec59205a1806bb6d70f263822
Jonathan Gray <jonmattgray@gmail.com>
2023-01-09 10:59:13 +0000

    use mapbox style coped from design

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,7 +2,7 @@ const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN || Window.config.mapboxToken || ''
 const MAPBOX_STYLE =
   process.env.MAPBOX_STYLE ||
   Window.config.mapboxStyle ||
-  'mapbox://styles/digicat-mapbox/clcj7eskl001q14o5bed5oam4'
+  'mapbox://styles/digicat-mapbox/clcja9aj6001d14o3yn7wvowa'
 const PUBLIC_BASE_PATH =
   process.env.PUBLIC_BASE_PATH || Window.config.publicBasePath || '/'
 
