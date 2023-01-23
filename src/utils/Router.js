3fa3551adb053cb76f6b58df103cd6be34bc391c
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2023-01-23 11:35:35 +0000

    Change BrowserRouter to HashRouter (#41)

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from '../pages/Home'
 import { PUBLIC_BASE_PATH } from './env'
 
 export default function Router() {
   return (
-    <BrowserRouter basename={PUBLIC_BASE_PATH}>
+    <HashRouter basename={PUBLIC_BASE_PATH}>
       <Routes>
         <Route exec path={'/'} element={<Home />} />
         <Route exec path={':projectId'} element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
